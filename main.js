97b379d692f0a7e1f1cf72873d18fa47dda8f6df
robs898 <rmackenzie8@gmail.com>
2017-09-17 00:50:56 +0100

    cleaner

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+function processData(cmcCoins) {
+  var btc_price = parseFloat(cmcCoins[0]['price_usd']);
+  var coinList = [];
+  getLocalCoins().forEach(function(localCoin) {
+    cmcCoins.forEach(function(cmcCoin) {
+      if (localCoin.symbol == cmcCoin.symbol) {
+        localCoin.name = cmcCoin.name;
+        localCoin.mkt_cap = parseFloat(cmcCoin.market_cap_usd);
+        localCoin.price_usd = parseFloat(cmcCoin.price_usd);
+        localCoin.price_btc = parseFloat(cmcCoin.price_btc);
+        if (localCoin.upper > localCoin.price_usd) {
+          localCoin.diff = (100 * ((localCoin.upper - localCoin.price_usd) / localCoin.upper));
+          coinList.push(localCoin);
+        };
+      };
+    });
+  });
+  getBuys(coinList, getTotalShare(coinList), btc_price);
+}
+
 function getLocalCoins() {
   var a = [];
   $('#coins input').each(function() {
@@ -15,27 +35,6 @@ function getTotalShare(coinList) {
     collectDiffs.push(coin.diff);
   });
   return collectDiffs.reduce(function(a, b) { return a + b; }, 0);
-}
-
-function createCoinList(cmcCoins) {
-  var btc_price = parseFloat(cmcCoins[0]['price_usd']);
-  var coinList = [];
-  getLocalCoins().forEach(function(localCoin) {
-    cmcCoins.forEach(function(cmcCoin) {
-      if (localCoin.symbol == cmcCoin.symbol) {
-        localCoin.name = cmcCoin.name;
-        localCoin.mkt_cap = parseFloat(cmcCoin.market_cap_usd);
-        localCoin.price_usd = parseFloat(cmcCoin.price_usd);
-        localCoin.price_btc = parseFloat(cmcCoin.price_btc);
-        if (localCoin.upper > localCoin.price_usd) {
-          localCoin.diff = (100 * ((localCoin.upper - localCoin.price_usd) / localCoin.upper));
-          coinList.push(localCoin);
-        };
-      };
-    });
-  });
-  var totalShare = getTotalShare(coinList);
-  getBuys(coinList, totalShare, btc_price);
 }
 
 function getBuys(coinList, totalShare, btc_price) {
@@ -63,7 +62,7 @@ function main() {
   $.ajax({
     url: "https://api.coinmarketcap.com/v1/ticker/?limit=200",
     success: function(response) {
-      createCoinList(response);
+      processData(response);
     }
   });
 }
