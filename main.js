840c49c6f9e4dbc6d7420bba5cf722fa1db8c86e
robs898 <rmackenzie8@gmail.com>
2017-09-18 18:17:49 +0100

    DOESNT IT LOOK NICE DAVE

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
+// main functions
+
+function main() {
+  $.ajax({
+    url: "https://api.coinmarketcap.com/v1/ticker/?limit=200",
+    success: function(response) {
+      processData(response);
+    }
+  });
+}
 function processData(cmcCoins) {
-  var btc_price = parseFloat(cmcCoins[0]['price_usd']);
+  var btcPrice = parseFloat(cmcCoins[0]['price_usd']);
+  var coinList = constructCoinList(cmcCoins);
+  var totalShare = getTotalShare(coinList);
+  var buyList = getBuys(coinList, totalShare, btcPrice);
+  var buyListSorted = sortList(buyList);
+  var buyListHtml = getBuyListHtml(buyListSorted);
+  renderBuyList(buyListHtml, 'output');
+}
+
+// private functions
+
+function constructCoinList(cmcCoins) {
+  var localCoins = getLocalCoins();
   var coinList = [];
-  getLocalCoins().forEach(function(localCoin) {
+  localCoins.forEach(function(localCoin) {
     cmcCoins.forEach(function(cmcCoin) {
       if (localCoin.symbol == cmcCoin.symbol) {
         localCoin.name = cmcCoin.name;
-        localCoin.mkt_cap = parseFloat(cmcCoin.market_cap_usd);
-        localCoin.price_usd = parseFloat(cmcCoin.price_usd);
-        localCoin.price_btc = parseFloat(cmcCoin.price_btc);
-        if (localCoin.upper > localCoin.price_usd) {
-          localCoin.diff = (100 * ((localCoin.upper - localCoin.price_usd) / localCoin.upper));
+        localCoin.priceUsd = parseFloat(cmcCoin.price_usd);
+        if (localCoin.upper > localCoin.priceUsd) {
+          localCoin.diff = (100 * ((localCoin.upper - localCoin.priceUsd) / localCoin.upper));
           coinList.push(localCoin);
         };
       };
     });
   });
-  getBuys(coinList, getTotalShare(coinList), btc_price);
+  return coinList
 }
 
 function getLocalCoins() {
   var a = [];
   $('#coins input').each(function() {
-    var d = {}
+    var d = {};
     d.symbol = this.id;
     d.upper = parseFloat(this.value);
     a.push(d);
   });
-  return a
+  return a;
 }
 
 function getTotalShare(coinList) {
@@ -37,32 +57,36 @@ function getTotalShare(coinList) {
   return collectDiffs.reduce(function(a, b) { return a + b; }, 0);
 }
 
-function getBuys(coinList, totalShare, btc_price) {
+function getBuys(coinList, totalShare, btcPrice) {
   var cash = document.getElementById("cash").value;
-  buyList = []
+  var buyList = [];
   coinList.forEach(function(coin) {
     coin.share = coin.diff / totalShare;
     coin.buy = coin.share * cash;
-    coin.buy_btc = coin.buy / btc_price;
-    buyList.push(coin.symbol + ' ' + coin.buy_btc);
-  })
-  outputBuyList(buyList);
+    coin.buy_btc = coin.buy / btcPrice;
+    buyList.push({
+      key: coin.symbol,
+      value: coin.buy_btc
+    });
+  });
+  return buyList;
 }
 
-function outputBuyList(buyList) {
-  document.getElementById("output").innerHTML = "";
-  buyList.forEach(function(coin) {
-    var output = document.createElement("li");
-    output.innerHTML = coin;
-    document.getElementById("output").appendChild(output);
+function sortList(list) {
+  return list.slice(0).sort(function(a, b) {
+    return b.value - a.value;
   });
 }
 
-function main() {
-  $.ajax({
-    url: "https://api.coinmarketcap.com/v1/ticker/?limit=200",
-    success: function(response) {
-      processData(response);
-    }
+function getBuyListHtml(buyListSorted) {
+  var html = '';
+  buyListSorted.forEach(function(coin) {
+    html += coin['key'] +' = ' + coin['value'] + '<br/>';
   });
+  return html
+}
+
+function renderBuyList(buyListHtml, id) {
+  document.getElementById(id).innerHTML = '';
+  document.getElementById(id).innerHTML = buyListHtml;
 }
